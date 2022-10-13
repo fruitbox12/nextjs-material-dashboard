4b6d8c3486b9c3539bea0a7408e91c63d4940dc1
Dylan <dylanwong007@gmail.com>
2022-10-13 00:05:45 -0400

    Update RTL.js

--- layouts/RTL.js
+++ layouts/RTL.js
@@ -15,8 +15,8 @@ import routes from "routes.js";
 
 import styles from "assets/jss/nextjs-material-dashboard/layouts/rtlStyle.js";
 
-import bgImage from "assets/img/sidebar-2.jpg";
-import logo from "assets/img/reactlogo.png";
+import bgImage from "assets/img/";
+import logo from "assets/img/";
 
 let ps;
 
